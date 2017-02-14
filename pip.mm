18eadbd3a4773f940a73858ead7a6e4d0a5ddfea
Yi Zhang <zhangyiant@hotmail.com>
2017-02-15 06:33:01 +0800

    init commit

---
+++ pip.mm
@@ -0,0 +1,11 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1484229622261" ID="ID_1421580500" MODIFIED="1484232842475" TEXT="pip">
+<node CREATED="1484229780428" ID="ID_689590027" MODIFIED="1484229786206" POSITION="right" TEXT="show package details">
+<node CREATED="1484229789642" ID="ID_1127727760" MODIFIED="1484229796384" TEXT="pip show &lt;package name&gt;"/>
+</node>
+<node CREATED="1484232844856" ID="ID_491915243" MODIFIED="1484232850957" POSITION="left" TEXT="search package">
+<node CREATED="1484232850962" ID="ID_122670614" MODIFIED="1484232859828" TEXT="pip search &quot;string&quot;"/>
+</node>
+</node>
+</map>
