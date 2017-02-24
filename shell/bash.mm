36c5577f65d3742cc8d70d88ba9f40ddc8e4b2cb
Yi Zhang <peter-yi.zhang@autodesk.com>
2017-02-24 17:24:49 +0800

    Add bash.mm

---
+++ shell/bash.mm
@@ -0,0 +1,32 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1487926920107" ID="ID_1744269414" MODIFIED="1487926940532" TEXT="bash">
+<node CREATED="1487926923533" ID="ID_1668419298" MODIFIED="1487926926716" POSITION="right" TEXT="https://www.gnu.org/software/bash/manual/bashref.html#Introduction-and-Notation"/>
+<node CREATED="1487926941479" ID="ID_1231076140" MODIFIED="1487926946561" POSITION="left" TEXT="Command Line Editing">
+<node CREATED="1487926983005" ID="ID_1461289169" MODIFIED="1487926990152" TEXT="Readline library"/>
+<node CREATED="1487927010747" ID="ID_551979097" MODIFIED="1487927024060" TEXT="enabled by default when using an interactive shell"/>
+<node CREATED="1487927026404" ID="ID_1205596693" MODIFIED="1487927043432" TEXT="--noediting to disable command line editing"/>
+<node CREATED="1487927044124" ID="ID_1581957171" MODIFIED="1487927192368" TEXT="also used in &quot;read -e&quot; builtin command"/>
+<node CREATED="1487927754245" ID="ID_431255911" MODIFIED="1487927761430" TEXT="Readline Bare Essentials">
+<node CREATED="1487927762045" ID="ID_1020648650" MODIFIED="1487927769239" TEXT="C-b move back one character"/>
+<node CREATED="1487927770021" ID="ID_1574663764" MODIFIED="1487927780645" TEXT="C-f move forward one character"/>
+<node CREATED="1487927781739" ID="ID_685557789" MODIFIED="1487927795575" TEXT="DEL or Backspace Delete the charater to the left of the cursor"/>
+<node CREATED="1487927796616" ID="ID_413878721" MODIFIED="1487927813258" TEXT="C-d Delete the character underneath the cursor"/>
+<node CREATED="1487927821815" ID="ID_1315203411" MODIFIED="1487927847121" TEXT="C-_ or C-x C-u Undo the last editing command. You can undo all the way back to an empty line."/>
+</node>
+<node CREATED="1487927904692" ID="ID_1650091318" MODIFIED="1487927911526" TEXT="Readline Movement Commands">
+<node CREATED="1487927913063" ID="ID_1064881757" MODIFIED="1487927919423" TEXT="C-a Move to the start of the line"/>
+<node CREATED="1487927920494" ID="ID_1292586403" MODIFIED="1487927928200" TEXT="C-e Move to the end of the line"/>
+<node CREATED="1487927929384" ID="ID_1395590666" MODIFIED="1487927948437" TEXT="M-f Move forward a word, where a word is composed of letters and digits"/>
+<node CREATED="1487927949870" ID="ID_333474058" MODIFIED="1487927956492" TEXT="M-b Move backward a word"/>
+<node CREATED="1487927957136" ID="ID_547792650" MODIFIED="1487927974166" TEXT="C-l Clear the screen, reprinting the current line at the top"/>
+</node>
+<node CREATED="1487928004759" ID="ID_1139901656" MODIFIED="1487928010738" TEXT="Readline Killing Commands">
+<node CREATED="1487928019363" ID="ID_1949047288" MODIFIED="1487928038482" TEXT="C-k Kill the text from the current cursor position to the end of the line."/>
+<node CREATED="1487928041322" ID="ID_1803189231" MODIFIED="1487928077159" TEXT="M-d Kill from the cursor to the end of the current word, or, if between words, to the end of the next word."/>
+<node CREATED="1487928078425" ID="ID_1933299916" MODIFIED="1487928113001" TEXT="M-DEL Kill from the cursor the start of the current word, or, if between words, to the start of the previous word."/>
+<node CREATED="1487928113995" ID="ID_1585433279" MODIFIED="1487928133734" TEXT="C-w Kill from the cursor to the previous whitespace."/>
+</node>
+</node>
+</node>
+</map>
