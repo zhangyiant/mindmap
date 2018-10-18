f9e9f71146cd88025c6731af908d6b15c4ca23d2
Peter-Yi Zhang <peter-yi.zhang@autodesk.com>
2018-10-18 13:09:10 +0800

    Add cpp primer mindmap.

---
+++ cpp/cpp primer.mm
@@ -0,0 +1,29 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1539741444822" ID="ID_1151547824" MODIFIED="1539741457077" TEXT="C++ Primer">
+<node CREATED="1539741515686" ID="ID_332657321" MODIFIED="1539741522178" POSITION="right" TEXT="Regular Expressions">
+<node CREATED="1539741546197" ID="ID_1695383163" MODIFIED="1539741564544" TEXT="A regular expression is a way of describing a sequence of characters."/>
+<node CREATED="1539762059910" ID="ID_1634439684" MODIFIED="1539762063552" TEXT="regex">
+<node CREATED="1539762063554" ID="ID_403846047" MODIFIED="1539762071799" TEXT="Class that represents a regular expression"/>
+</node>
+<node CREATED="1539762074223" ID="ID_337995716" MODIFIED="1539762076544" TEXT="regex_match">
+<node CREATED="1539762076546" ID="ID_659780449" MODIFIED="1539762092614" TEXT="Matches a sequence of characters against a regular expression"/>
+</node>
+<node CREATED="1539762094334" ID="ID_1137370189" MODIFIED="1539762098668" TEXT="regex_search">
+<node CREATED="1539762098669" ID="ID_1509348722" MODIFIED="1539762111525" TEXT="Finds the first subseqeuence that matches the regular expression"/>
+</node>
+<node CREATED="1539762113150" ID="ID_429735800" MODIFIED="1539762117173" TEXT="regex_replace">
+<node CREATED="1539762117174" ID="ID_1277324016" MODIFIED="1539762132997" TEXT="Replaces a regular expression using a given format"/>
+</node>
+<node CREATED="1539762136437" ID="ID_1643900761" MODIFIED="1539762142858" TEXT="sregex_iterator">
+<node CREATED="1539762142859" ID="ID_1912643135" MODIFIED="1539762164963" TEXT="Iterator adaptor that calls regex_search to iterate through the matches in a string"/>
+</node>
+<node CREATED="1539762168020" ID="ID_1253597666" MODIFIED="1539762169929" TEXT="smatch">
+<node CREATED="1539762169930" ID="ID_1609043349" MODIFIED="1539762183147" TEXT="Container class that holds the results of searching a string"/>
+</node>
+<node CREATED="1539762184604" ID="ID_124006514" MODIFIED="1539762186810" TEXT="ssub_match">
+<node CREATED="1539762186811" ID="ID_1031458412" MODIFIED="1539762200506" TEXT="Results for a matched subexpression in a string"/>
+</node>
+</node>
+</node>
+</map>
