671962f41df229ad17d4b89fa94772ce161bb86d
Peter Zhang <peter-yi.zhang@autodesk.com>
2019-04-16 10:20:25 +0800

    Add Firefox.

---
+++ Firefox.mm
@@ -0,0 +1,10 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1555381109025" ID="ID_1479473888" MODIFIED="1555381114026" TEXT="Firefox">
+<node CREATED="1555381123025" ID="ID_536004493" MODIFIED="1555381126355" POSITION="right" TEXT="shortcut">
+<node CREATED="1555381131335" ID="ID_1833733705" MODIFIED="1555381133276" TEXT="Stop">
+<node CREATED="1555381133735" ID="ID_1818333719" MODIFIED="1555381135907" TEXT="Esc"/>
+</node>
+</node>
+</node>
+</map>
