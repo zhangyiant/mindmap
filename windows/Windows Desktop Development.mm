3ca96459b49b5d7284d904b1f26c5a1449ad78e7
Yi Zhang <zhangyiant@hotmail.com>
2021-06-05 10:03:09 +0800

    Update

---
+++ windows/Windows Desktop Development.mm
@@ -0,0 +1,9 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1620572324296" ID="ID_688678089" MODIFIED="1620572331993" TEXT="Windows Desktop Development">
+<node CREATED="1620572333098" ID="ID_1664084475" MODIFIED="1620572354991" POSITION="right" TEXT="UWP(Universal Windows Platform)"/>
+<node CREATED="1620572356321" ID="ID_297823412" MODIFIED="1620572371973" POSITION="right" TEXT="WPF(Windows Presentation Foundation"/>
+<node CREATED="1620572372506" ID="ID_432686741" MODIFIED="1620572376157" POSITION="right" TEXT="Windows Forms"/>
+<node CREATED="1620572379842" ID="ID_1166386614" MODIFIED="1620572384703" POSITION="left" TEXT="C++(Win32)"/>
+</node>
+</map>
